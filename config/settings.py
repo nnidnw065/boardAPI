ec54c1116b4e61dcd6c754fd413916cd5edd9480
hermes <nnidnw065@gmail.com>
2022-06-25 16:57:34 +0900

    accounts app

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'board',
     'rest_framework',
+    'board',
+    'accounts',
 ]
 
 MIDDLEWARE = [
