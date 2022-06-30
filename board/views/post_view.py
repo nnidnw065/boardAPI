553c9228585c32a1e18ce5c5e8fe31228971eef1
hermes <nnidnw065@gmail.com>
2022-06-30 16:54:38 +0900

    update

--- board/views/post_view.py
+++ board/views/post_view.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 from ..models import Post
 from ..serializers import PostSerializer
 from rest_framework import status
